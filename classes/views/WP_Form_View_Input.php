51bf18523e6bb99ce0a7b0ad7a88497e91d5a5b1
emeraldjava <paul.t.oconnell@gmail.com>
2014-01-13 16:23:40 +0000

    handle radio options specifically

--- classes/views/WP_Form_View_Input.php
+++ classes/views/WP_Form_View_Input.php
@@ -14,8 +14,10 @@ class WP_Form_View_Input extends WP_Form_View {
 	protected function input( WP_Form_Element $element ) {
 		$attributes = $element->get_all_attributes();
 		$attributes = WP_Form_View::prepare_attributes($attributes);
-		// POC
-		if($element->get_type()!='hidden')
+		if($element->get_type()=='radio'){
+			$template = '<input %s />';
+		}
+		else if($element->get_type()=='text'||$element->get_type()=='number')
 			$template = '<div class="col-md-8"><input %s /></div>';
 		else
 			$template = '<input %s />';
